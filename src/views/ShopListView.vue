05178e1e9c7ff883db797d0ed7ce7d98fdad9fbf
wenchi7 <syz58171020@gmail.com>
2025-08-24 15:29:42 +0800

    feat: display lists info

--- src/views/ShopListView.vue
+++ src/views/ShopListView.vue
@@ -1,3 +1,24 @@
 <template>
-  <div>ShopListView</div>
+  <div class="flex flex-col mt-4">
+    <button
+      @click="createList"
+      class="flex justify-center items-center font-semibold text-slate-900 px-2 py-1 mx-auto bg-red-500 hover:bg-red-600 w-36 border border-black rounded-full my-4 text-2xl md:text-3xl"
+    >
+      新增明細
+    </button>
+    <div>
+      <ShopList />
+    </div>
+  </div>
 </template>
+
+<script setup lang="ts">
+import ShopList from '@/components/ShopListPage/ShopList.vue'
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+
+const createList = () => {
+  router.push('/home/createlist')
+}
+</script>
